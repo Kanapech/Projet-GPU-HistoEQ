16e03f04933023998b19dd03d1cb86ef5d82ab5f
Kanapech <silver36tv@gmail.com>
2021-12-02 23:56:11 +0100

    added time for seq functions

--- main.cu
+++ main.cu
@@ -4,6 +4,7 @@
 
 #include "image.hpp"
 #include "seqFunctions.hpp"
+#include "utils/chronoCPU.hpp"
 
 using namespace std;
 
@@ -15,14 +16,25 @@ int main(int argc, char **argv){
     float htab[size], stab[size], vtab[size];
 
     //cout << strlen((char*)img._pixels) << endl;
+    ChronoCPU chr;
+	chr.start();
     rgb2hsvCPU(img._pixels, htab, stab, vtab, img._width, img._height);
+    chr.stop();
+    cout << "rgb2hsvCPU : " << chr.elapsedTime() << "ms" << endl;
+
+	chr.start();
     unsigned char* pix = hsv2rgbCPU(htab, stab, vtab, img._width, img._height);
+    chr.stop();
+    cout << "hsv2rgbCPU : " << chr.elapsedTime() << "ms" << endl;
 
     Image im(img._width, img._height, 3);
     im._pixels = pix;
     im.save("output.png");
 
+	chr.start();
     int* hist = histogramCPU(vtab, size);
+    chr.stop();
+    cout << "histoCPU : " << chr.elapsedTime() << "ms" << endl;
 
     return 0;
 }
